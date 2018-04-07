42b9a47727820c4adf5b3181e38cafe3fe39c0e9
Francesca <ringot.france@gmail.com>
2018-04-07 10:46:55 +0200

    route mocks

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
-const http = require('http')
+const express = require('express')
+const member1 = require('../mocks/mock1.json')
+const member2 = require('../mocks/mock2.json')
 
-const server = http.createServer((request, response) => {
-	console.log("j'ai une requete yey", request.url)
-	response.end('OK')
+const members = [ member1, member2]
+console.log(members)
+
+const app = express()
+
+app.get('/', (request, response) => {
+	response.send('OK')
 })
 
-server.listen(3240, () => console.log("j'écoute sur le port 3240"))
+app.get('/members', (request, response) => {
+	response.json(members)
+
+})
+
+app.listen(3240, () => console.log("j'écoute sur le port 3240"))
